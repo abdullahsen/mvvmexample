55edbdf2bfb488190f89f7907ffc542872672435
Abdullah Sen <sena@Abdullahs-MBP.home>
2022-05-01 18:23:50 +0200

    Add fragments for initial and detail screens

---
+++ app/src/androidTest/java/com/asen/mvvmexample/ui/view/WeatherScreenTest.kt
@@ -0,0 +1,5 @@
+package com.asen.mvvmexample.ui.view
+
+import org.junit.Assert.*
+
+class WeatherScreenTest
--- app/src/main/java/com/asen/mvvmexample/ui/view/MainActivity.kt
+++ app/src/main/java/com/asen/mvvmexample/ui/view/MainActivity.kt
@@ -3,30 +3,14 @@ package com.asen.mvvmexample.ui.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.asen.mvvmexample.databinding.ActivityMainBinding
-import com.asen.mvvmexample.ui.viewmodel.WeatherViewModel
-import com.asen.mvvmexample.ui.viewmodel.WeatherViewModelUIState
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: WeatherViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initObservers()
-        viewModel.loadWeather()
-    }
-
-    private fun initObservers() {
-        viewModel.uiState.observe(this) { renderUIState(it) }
-    }
-
-    private fun renderUIState(uiState: WeatherViewModelUIState) = when (uiState) {
-        is WeatherViewModelUIState.ShowError -> binding.textView.text = "ERROR"
-        is WeatherViewModelUIState.ShowLoading -> binding.textView.text = "LOADING"
-        is WeatherViewModelUIState.WeatherLoaded -> binding.textView.text = uiState.weather.name
     }
 }
